c561694e121bda3438c284592922884b29e1e404
Ben Maddox <benwmaddox@gmail.com>
2020-10-31 12:29:28 -0400

    Trying to setup a context

--- context-parser.js
+++ context-parser.js
@@ -3,22 +3,43 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ContextParser = exports.BaseContext = void 0;
 var wasm_structure_1 = require("./wasm-structure");
 exports.BaseContext = {
-    "export": { inputTypes: undefined, outputTypes: undefined, OpsCodes: undefined },
-    "fn": { inputTypes: undefined, outputTypes: undefined, OpsCodes: undefined, newContext: true },
-    ";": { inputTypes: undefined, outputTypes: undefined, OpsCodes: [wasm_structure_1.Opcodes.end], popContext: true },
-    '+': { inputTypes: ['int', 'int'], outputTypes: ['int'], OpsCodes: [wasm_structure_1.Opcodes.i32Add] },
-    '*': { inputTypes: ['int', 'int'], outputTypes: ['int'], OpsCodes: [wasm_structure_1.Opcodes.i32Mul] },
-    '-': { inputTypes: ['int', 'int'], outputTypes: ['int'], OpsCodes: [wasm_structure_1.Opcodes.i32Sub] },
-    '<': { inputTypes: ['int', 'int'], outputTypes: ['int'], OpsCodes: [wasm_structure_1.Opcodes.i32LessThanSigned] },
-    '==': { inputTypes: ['int', 'int'], outputTypes: ['bool'], OpsCodes: [wasm_structure_1.Opcodes.i32Equals] },
-    '==0': { inputTypes: ['int'], outputTypes: ['bool'], OpsCodes: [wasm_structure_1.Opcodes.i32EqualsZero] },
-    '&&': { inputTypes: ['int', 'int'], outputTypes: ['bool'], OpsCodes: [wasm_structure_1.Opcodes.i32And] }
+    "export": { inputTypes: undefined, outputTypes: undefined, opCodes: undefined },
+    "fn": { inputTypes: undefined, outputTypes: undefined, opCodes: undefined, newContext: true },
+    "var": { inputTypes: undefined, outputTypes: undefined, opCodes: undefined, newContext: true },
+    ";": { inputTypes: undefined, outputTypes: undefined, opCodes: [wasm_structure_1.Opcodes.end], popContext: true },
+    '+': { inputTypes: ['int', 'int'], outputTypes: ['int'], opCodes: [wasm_structure_1.Opcodes.i32Add] },
+    '*': { inputTypes: ['int', 'int'], outputTypes: ['int'], opCodes: [wasm_structure_1.Opcodes.i32Mul] },
+    '-': { inputTypes: ['int', 'int'], outputTypes: ['int'], opCodes: [wasm_structure_1.Opcodes.i32Sub] },
+    '<': { inputTypes: ['int', 'int'], outputTypes: ['int'], opCodes: [wasm_structure_1.Opcodes.i32LessThanSigned] },
+    '==': { inputTypes: ['int', 'int'], outputTypes: ['bool'], opCodes: [wasm_structure_1.Opcodes.i32Equals] },
+    '==0': { inputTypes: ['int'], outputTypes: ['bool'], opCodes: [wasm_structure_1.Opcodes.i32EqualsZero] },
+    '&&': { inputTypes: ['int', 'int'], outputTypes: ['bool'], opCodes: [wasm_structure_1.Opcodes.i32And] },
 };
 var ContextParser = /** @class */ (function () {
     function ContextParser() {
     }
     ContextParser.prototype.parse = function (context, words) {
-        return {};
+        if (words.length == 0)
+            return null;
+        var nextWord = words[0];
+        if (nextWord.startsWith("'")) {
+            console.log(nextWord);
+        }
+        else if (nextWord.startsWith("\"")) {
+            console.log(nextWord);
+        }
+        else {
+            // Lookup through context
+            if (context[nextWord]) {
+                console.log(nextWord);
+                console.log(context[nextWord]);
+            }
+            else {
+                // number? 
+                console.log(nextWord);
+            }
+        }
+        return this.parse(context, words.slice(1));
     };
     return ContextParser;
 }());
--- context-parser.ts
+++ context-parser.ts
@@ -2,21 +2,51 @@ import { Opcodes } from './wasm-structure';
 import { ASTFunction, ASTImport, ASTModule, FunctionParser, ModuleParser, ASTParameter } from './intermediate-structure';
 import { match } from 'assert';
 
-export var BaseContext = {
-    "export": { inputTypes: undefined, outputTypes: undefined, OpsCodes: undefined },
-    "fn": { inputTypes: undefined, outputTypes: undefined, OpsCodes: undefined, newContext: true },
-    ";": { inputTypes: undefined, outputTypes: undefined, OpsCodes: [Opcodes.end], popContext: true },
-    '+': { inputTypes: ['int', 'int'], outputTypes: ['int'], OpsCodes: [Opcodes.i32Add] },
-    '*': { inputTypes: ['int', 'int'], outputTypes: ['int'], OpsCodes: [Opcodes.i32Mul] },
-    '-': { inputTypes: ['int', 'int'], outputTypes: ['int'], OpsCodes: [Opcodes.i32Sub] },
-    '<': { inputTypes: ['int', 'int'], outputTypes: ['int'], OpsCodes: [Opcodes.i32LessThanSigned] },
-    '==': { inputTypes: ['int', 'int'], outputTypes: ['bool'], OpsCodes: [Opcodes.i32Equals] },
-    '==0': { inputTypes: ['int'], outputTypes: ['bool'], OpsCodes: [Opcodes.i32EqualsZero] },
-    '&&': { inputTypes: ['int', 'int'], outputTypes: ['bool'], OpsCodes: [Opcodes.i32And] }
+export type ContextItem = {
+    inputTypes?: Array<string>,
+    outputTypes?: Array<string>,
+    opCodes?: Array<Opcodes>,
+    newContext?: boolean,
+    popContext?: boolean
+}
+
+export var BaseContext: { [index: string]: ContextItem } = {
+    "export": { inputTypes: undefined, outputTypes: undefined, opCodes: undefined },
+    "fn": { inputTypes: undefined, outputTypes: undefined, opCodes: undefined, newContext: true },
+    "var": { inputTypes: undefined, outputTypes: undefined, opCodes: undefined, newContext: true },
+    ";": { inputTypes: undefined, outputTypes: undefined, opCodes: [Opcodes.end], popContext: true },
+    '+': { inputTypes: ['int', 'int'], outputTypes: ['int'], opCodes: [Opcodes.i32Add] },
+    '*': { inputTypes: ['int', 'int'], outputTypes: ['int'], opCodes: [Opcodes.i32Mul] },
+    '-': { inputTypes: ['int', 'int'], outputTypes: ['int'], opCodes: [Opcodes.i32Sub] },
+    '<': { inputTypes: ['int', 'int'], outputTypes: ['int'], opCodes: [Opcodes.i32LessThanSigned] },
+    '==': { inputTypes: ['int', 'int'], outputTypes: ['bool'], opCodes: [Opcodes.i32Equals] },
+    '==0': { inputTypes: ['int'], outputTypes: ['bool'], opCodes: [Opcodes.i32EqualsZero] },
+    '&&': { inputTypes: ['int', 'int'], outputTypes: ['bool'], opCodes: [Opcodes.i32And] },
+
 };
 
 export class ContextParser {
     parse(context: any, words: Array<string>): any {
-        return {};
+        if (words.length == 0) return null;
+
+        var nextWord = words[0];
+        if (nextWord.startsWith("'")) {
+            console.log(nextWord)
+        }
+        else if (nextWord.startsWith("\"")) {
+            console.log(nextWord)
+        }
+        else {
+            // Lookup through context
+            if (context[nextWord]) {
+                console.log(nextWord);
+                console.log(context[nextWord]);
+            }
+            else {
+                // number? 
+                console.log(nextWord);
+            }
+        }
+        return this.parse(context, words.slice(1));
     }
 }
